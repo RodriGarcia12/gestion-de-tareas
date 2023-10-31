2b94b80b4acffe6b9f791a7aeeb2d78aaa6fecd6
Rodrigo <rgarciaa125@gmail.com>
2023-10-30 22:35:03 -0300

    primer commit con migrations

---
+++ database/migrations/2023_10_31_010330_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('user-assigned_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('author_id', 'user-assigned_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
