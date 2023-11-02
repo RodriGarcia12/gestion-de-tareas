f528d92f0b42749bfd63ae360d3edfe6e01a3d98
Rodrigo <rgarciaa125@gmail.com>
2023-11-01 21:27:39 -0300

    creacion y finalizacion de seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Task;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'id' => '1',
+            'name' => 'Pedro',
+            'email' => 'pedrito@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+       User::create([
+            'id' => '2',
+            'name' => 'Franco',
+            'email' => 'franquito@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        User::create([
+            'id' => '3',
+            'name' => 'Rodrigo',
+            'email' => 'rodri@gmail.com',
+            'password' => Hash::make('1234')
+        ]);
+
+        Task::create([
+            'id' => '1',
+            'title' => 'Tarea con id 1',
+            'body' => 'Cuerpo de la tarea con id 1 asignado a usuario 1',
+            'author_id' => '3',
+            'user-assigned_id' => '1'
+        ]);
     }
 }
