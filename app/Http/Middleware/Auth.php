9316ad6bc15316ac2e35b9d50d8d1335cdffe823
Rodrigo <rgarciaa125@gmail.com>
2023-11-06 21:58:11 -0300

    middleware para auth

---
+++ app/Http/Middleware/Auth.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class Auth
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $tokenHeader = [
+            "Authorization" => $request -> header("Authorization"),
+            "Accept" => "application/json",
+            "Content-Type" => "application/json"
+        ];
+
+        $response = Http::withHeaders($tokenHeader) -> get ( "http://localhost:8000/api/v1/validate");
+        if($response -> successful()){
+            return $next($request);
+        }
+        return response(["message" => "No autorizado"], 401);
+    }
+}
