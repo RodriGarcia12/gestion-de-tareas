3cfda6ca85d5669fd30bccc4e18863814c930f9c
Rodrigo <rgarciaa125@gmail.com>
2023-11-06 21:59:34 -0300

    se agrega middleware

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Middleware\Autenticacion;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,13 @@ use App\Http\Controllers\TaskController;
 |
 */
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::prefix('v1')->group(function () {
-    Route::post('/task',[TaskController::class,'Create']);
-    Route::get('/task',[TaskController::class,'Read']);
+    Route::post('/task',[TaskController::class,'Create'])->middleware(Auth::class);
+    Route::get('/task',[TaskController::class,'Read'])->middleware(Auth::class);
     Route::get('/task/{d}',[TaskController::class,'Find']);
     Route::put('/task/{d}',[TaskController::class,'Update']);
     Route::delete('/task/{d}',[TaskController::class,'Delete']);
