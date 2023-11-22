987526e299b8b2462fac05c7801297760ac9481e
Rodrigo <rgarciaa125@gmail.com>
2023-11-21 23:42:56 -0300

    se cambia use

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
-use App\Http\Middleware\Autenticacion;
+use App\Http\Middleware\Auth;
 
 /*
 |--------------------------------------------------------------------------
